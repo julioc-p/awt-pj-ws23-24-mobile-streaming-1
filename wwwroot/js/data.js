d180722996001fe1009af7558fe8ccc09137171e
juliop1980 <14-10820@usb.ve>
2023-12-13 22:55:48 +0100

    built mpd file

--- wwwroot/js/data.js
+++ wwwroot/js/data.js
@@ -135,7 +135,7 @@ function playbackPaused() {
 // init function to run on pageload 
 function init() {
     // https://media.axprod.net/TestVectors/v7-Clear/Manifest_MultiPeriod.mpd
-    var url = "bbb-manifest-refresh.mpd";
+    var url = "content/random.mpd";
     var videoElement = document.querySelector(".videoContainer video");
     var player = dashjs.MediaPlayer().create();
     dashjsPlayer = player;
