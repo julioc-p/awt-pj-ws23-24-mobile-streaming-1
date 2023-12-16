9d20fef86ba51e9671dec26e7a517f33bac017de
juliop1980 <14-10820@usb.ve>
2023-12-16 22:52:52 +0100

    generates two plots: average power consumption by setting and avg power consumption by resolution

--- PythonScripts/analysis_script.py
+++ PythonScripts/analysis_script.py
@@ -1,46 +1,96 @@
+# analysis_script.py
+
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Path to the directory containing the data files
-data_directory = '../Measurements/data'
-
-# Function to read and process a single data file
 def process_data(file_path):
     df = pd.read_csv(file_path, delimiter=';')
     avg_power = df[df['VideoPlaying'] == True]['TotalPower'].mean()
     return avg_power
 
-# Process all data files in the directory
-all_data = []
-for filename in os.listdir(data_directory):
-    if filename.startswith('random'):
-        file_path = os.path.join(data_directory, filename)
-        settings = filename.split('_')[1:]  # Extracting codec, resolution, bitrate, fps from filename
-        settings_str = '_'.join(settings)
-        avg_power = process_data(file_path)
-        all_data.append({'Settings': settings_str, 'TotalPower': avg_power})
+def generate_insight_by_settings(data_directory):
+    all_data = []
+    for filename in os.listdir(data_directory):
+        if filename.startswith('random'):
+            file_path = os.path.join(data_directory, filename)
+            settings = filename.split('_')[1:]  # Extracting codec, resolution, bitrate, fps from filename
+            settings_str = '_'.join(settings)
+            avg_power = process_data(file_path)
+            all_data.append({'Settings': settings_str, 'TotalPower': avg_power})
 
-# Create DataFrame from the list of dictionaries
-result_df = pd.DataFrame(all_data)
+    if not all_data:
+        print("No data found.")
+        return
 
-# Extract individual settings into separate columns
-result_df[['Codec', 'Resolution', 'Bitrate', 'FPS']] = result_df['Settings'].str.split('_', expand=True)
+    # Create DataFrame from the list of dictionaries
+    result_df = pd.DataFrame(all_data)
 
-# Define custom sorting order for 'Resolution' column
-resolution_order = ['HD', '4K']
-result_df['Resolution'] = pd.Categorical(result_df['Resolution'], categories=resolution_order, ordered=True)
+    # Extract individual settings into separate columns
+    result_df[['Codec', 'Resolution', 'Bitrate', 'FPS']] = result_df['Settings'].str.split('_', expand=True)
 
-# Sort DataFrame by 'Resolution' and 'Bitrate'
-result_df = result_df.sort_values(by=['Resolution', 'Bitrate'])
+    # Sort by settings for better visualization
+    result_df = result_df.sort_values(by='Settings')
 
-# Plotting using seaborn
-plt.figure(figsize=(16, 8))  # Increase the width of the plot
-sns.barplot(x='Settings', y='TotalPower', data=result_df)
-plt.title('Average Power Consumption for Different Settings')
-plt.xlabel('Settings (Codec_Resolution_Bitrate_FPS)')
-plt.ylabel('Average Power Consumption')
-plt.xticks(rotation=45, ha='right')  # Adjust rotation and horizontal alignment
-plt.tight_layout()  # Ensure proper layout
-plt.show()
+    # Plotting using seaborn
+    plt.figure(figsize=(15, 8))  # Adjusted figure size
+    sns.barplot(x='Settings', y='TotalPower', data=result_df)
+    plt.title('Average Power Consumption for Different Settings')
+    plt.xlabel('Settings (Codec_Resolution_Bitrate_FPS)')
+    plt.ylabel('Average Power Consumption')
+    plt.xticks(rotation=45, ha='right')  # Adjust rotation and alignment
+    plt.tight_layout()  # Improved spacing
+    plt.show()
+
+def generate_insight_by_resolution(data_directory):
+    def process_file(file_path):
+        df = pd.read_csv(file_path, delimiter=';')
+        sum_power = df['TotalPower'].sum()
+        return sum_power, len(df)
+
+    def process_directory(directory_path):
+        resolutions = set()
+        sum_powers = {}
+
+        for filename in os.listdir(directory_path):
+            if filename.startswith('random'):
+                file_path = os.path.join(directory_path, filename)
+                resolution = filename.split('_')[1]
+                resolutions.add(resolution)
+
+                sum_power, num_data_points = process_file(file_path)
+                if resolution not in sum_powers:
+                    sum_powers[resolution] = []
+                sum_powers[resolution].append([sum_power, num_data_points])
+
+        return resolutions, sum_powers
+
+    def plot_avg_power(resolutions, sum_powers):
+        for resolution in resolutions:
+            sum_resolution = 0
+            num_data_points = 0
+            for power, data_points in sum_powers[resolution]:
+                sum_resolution += power
+                num_data_points += data_points
+            plt.bar(resolution, (sum_resolution / num_data_points), label=resolution)
+
+        plt.xlabel('Resolution')
+        plt.ylabel('Average Power Consumption')
+        plt.title('Average Power Consumption for Different Resolutions')
+        plt.legend()
+        plt.show()
+
+    # Process files in the given directory
+    resolutions, sum_powers = process_directory(data_directory)
+
+    # Plot the average power for each resolution
+    plot_avg_power(resolutions, sum_powers)
+
+if __name__ == "__main__":
+    # Path to the directory containing the data files
+    data_directory = '../Measurements/data'
+
+    # Generate insights
+    generate_insight_by_settings(data_directory)
+    generate_insight_by_resolution(data_directory)
