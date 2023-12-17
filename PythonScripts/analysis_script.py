83274dd74660ab0b29ec551673ac951c2e0b2fb6
juliop1980 <14-10820@usb.ve>
2023-12-17 10:21:39 +0100

    added new plot for avgs across combinations of bitrate x codec and restructured code

--- PythonScripts/analysis_script.py
+++ PythonScripts/analysis_script.py
@@ -10,82 +10,78 @@ def process_data(file_path):
     avg_power = df[df['VideoPlaying'] == True]['TotalPower'].mean()
     return avg_power
 
-def generate_insight_by_settings(data_directory):
-    all_data = []
-    for filename in os.listdir(data_directory):
+def process_directory(directory_path, filename_condition):
+    data_combinations = set()
+    sum_powers = {}
+
+    for filename in os.listdir(directory_path):
         if filename.startswith('random'):
-            file_path = os.path.join(data_directory, filename)
-            settings = filename.split('_')[1:]  # Extracting codec, resolution, bitrate, fps from filename
-            settings_str = '_'.join(settings)
-            avg_power = process_data(file_path)
-            all_data.append({'Settings': settings_str, 'TotalPower': avg_power})
+            file_path = os.path.join(directory_path, filename)
+            combination = filename_condition(file_path)
+            data_combinations.add(combination)
 
-    if not all_data:
-        print("No data found.")
-        return
+            sum_power, num_data_points = process_file(file_path)
+            if combination not in sum_powers:
+                sum_powers[combination] = []
+            sum_powers[combination].append([sum_power, num_data_points])
 
-    # Create DataFrame from the list of dictionaries
-    result_df = pd.DataFrame(all_data)
+    return data_combinations, sum_powers
 
-    # Extract individual settings into separate columns
-    result_df[['Codec', 'Resolution', 'Bitrate', 'FPS']] = result_df['Settings'].str.split('_', expand=True)
+def process_file(file_path):
+    df = pd.read_csv(file_path, delimiter=';')
+    sum_power = df['TotalPower'].sum()
+    return sum_power, len(df)
 
-    # Sort by settings for better visualization
-    result_df = result_df.sort_values(by='Settings')
+def plot_avg_power(data_combinations, sum_powers, x_label, plot_title):
+    data_combinations = sorted(data_combinations)
+    for combination in data_combinations:
+        sum_combination = 0
+        num_data_points = 0
+        for power, data_points in sum_powers[combination]:
+            sum_combination += power
+            num_data_points += data_points
+        plt.bar(f'{combination}', (sum_combination / num_data_points), label=f'{combination}')
 
-    # Plotting using seaborn
-    plt.figure(figsize=(15, 8))  # Adjusted figure size
-    sns.barplot(x='Settings', y='TotalPower', data=result_df)
-    plt.title('Average Power Consumption for Different Settings')
-    plt.xlabel('Settings (Codec_Resolution_Bitrate_FPS)')
+    plt.xlabel(x_label)
     plt.ylabel('Average Power Consumption')
-    plt.xticks(rotation=45, ha='right')  # Adjust rotation and alignment
-    plt.tight_layout()  # Improved spacing
+    plt.title(plot_title)
+    plt.legend()
     plt.show()
 
-def generate_insight_by_resolution(data_directory):
-    def process_file(file_path):
-        df = pd.read_csv(file_path, delimiter=';')
-        sum_power = df['TotalPower'].sum()
-        return sum_power, len(df)
-
-    def process_directory(directory_path):
-        resolutions = set()
-        sum_powers = {}
-
-        for filename in os.listdir(directory_path):
-            if filename.startswith('random'):
-                file_path = os.path.join(directory_path, filename)
-                resolution = filename.split('_')[1]
-                resolutions.add(resolution)
-
-                sum_power, num_data_points = process_file(file_path)
-                if resolution not in sum_powers:
-                    sum_powers[resolution] = []
-                sum_powers[resolution].append([sum_power, num_data_points])
-
-        return resolutions, sum_powers
-
-    def plot_avg_power(resolutions, sum_powers):
-        for resolution in resolutions:
-            sum_resolution = 0
-            num_data_points = 0
-            for power, data_points in sum_powers[resolution]:
-                sum_resolution += power
-                num_data_points += data_points
-            plt.bar(resolution, (sum_resolution / num_data_points), label=resolution)
-
-        plt.xlabel('Resolution')
-        plt.ylabel('Average Power Consumption')
-        plt.title('Average Power Consumption for Different Resolutions')
-        plt.legend()
-        plt.show()
+def generate_insight_by_settings(data_directory):
+    def filename_condition(filename):
+        settings = filename.split('_')[1:]
+        return '_'.join(settings)
 
     # Process files in the given directory
-    resolutions, sum_powers = process_directory(data_directory)
+    settings_combinations, sum_powers = process_directory(data_directory, filename_condition)
+
+    # Plot the average power for each combination of settings
+    plot_avg_power(settings_combinations, sum_powers, 'Settings (Codec_Resolution_Bitrate_FPS)',
+                   'Average Power Consumption for Different Settings')
+
+def generate_insight_by_resolution(data_directory):
+    def filename_condition(filename):
+        return filename.split('_')[1]
+
+    # Process files in the given directory
+    resolutions, sum_powers = process_directory(data_directory, filename_condition)
 
     # Plot the average power for each resolution
-    plot_avg_power(resolutions, sum_powers)
+    plot_avg_power(resolutions, sum_powers, 'Resolution',
+                   'Average Power Consumption for Different Resolutions')
+
+def generate_insight_by_codec_bitrate(data_directory):
+    def filename_condition(filename):
+        codec, bitrate = filename.split('_')[3:5]
+        return f'{codec} x {bitrate}'
+
+    # Process files in the given directory
+    codec_bitrate_combinations, sum_powers = process_directory(data_directory, filename_condition)
+
+    # Plot the average power for each combination of codec and bitrate
+    plot_avg_power(codec_bitrate_combinations, sum_powers, 'Codec x Bitrate',
+                   'Average Power Consumption for Different Codec x Bitrate Combinations')
 
 if __name__ == "__main__":
     # Path to the directory containing the data files
@@ -94,3 +90,4 @@ if __name__ == "__main__":
     # Generate insights
     generate_insight_by_settings(data_directory)
     generate_insight_by_resolution(data_directory)
+    generate_insight_by_codec_bitrate(data_directory)
