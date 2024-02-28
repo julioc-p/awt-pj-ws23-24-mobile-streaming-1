49d8d04aa0790f85947b0610c9a64a8a42fe037f
juliop1980 <14-10820@usb.ve>
2024-02-28 09:21:29 +0100

    overall insights

--- PythonScripts/analysis_script.py
+++ PythonScripts/analysis_script.py
@@ -22,6 +22,24 @@ def process_directory(directory_path, filename_condition):
 
     return data_combinations, sum_powers
 
+def process_directory_all(directory_path, filename_condition):
+    data_combinations = set()
+    sum_powers = {}
+
+    for directory in os.listdir(directory_path):
+        directory_path_aux = os.path.join(directory_path, directory)
+        for filename in os.listdir(directory_path_aux):
+            file_path = os.path.join(directory_path_aux, filename)
+            combination = filename_condition(filename)
+            data_combinations.add(combination)
+
+            sum_power, num_data_points = process_file(file_path)
+            if combination not in sum_powers:
+                sum_powers[combination] = []
+            sum_powers[combination].append([sum_power, num_data_points])
+
+    return data_combinations, sum_powers
+
 def normalize_codec(codec):
     if codec.startswith('avc'):
         return 'H264'
@@ -72,6 +90,7 @@ def plot_avg_power(avg_powers, x_label, plot_title, directory_name):
     ax.set_title("\n".join(wrap(plot_title, 50)))
     lgd = plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     # turn plot into a png file
+    directory_name = directory_name if directory_name else 'overall'
     plt.savefig(f'{directory_name}_{plot_title}.png', bbox_inches='tight')
     plt.clf()
 
@@ -102,7 +121,7 @@ def plot_corr_heat_map(df, directory_name):
     plt.savefig(f'{directory_name}_correlation_heat_map.png')
     plt.clf()
 
-def generate_insight_by_settings(data_directory, directory_name):
+def generate_insight_by_settings(data_directory, directory_name = None):
     """
     Generates insights based on the settings of the data files in the given directory.
 
@@ -119,7 +138,7 @@ def generate_insight_by_settings(data_directory, directory_name):
         return '_'.join(settings)
 
     # Process files in the given directory
-    settings_combinations, sum_powers = process_directory(data_directory, filename_condition)
+    settings_combinations, sum_powers = process_directory(data_directory, filename_condition) if directory_name else process_directory_all(data_directory, filename_condition)
     avg_powers = get_avg_power(settings_combinations, sum_powers)
     # Plot the average power for each combination of settings
     plot_avg_power(avg_powers, 'Settings (Codec_Resolution_Bitrate_FPS)',
@@ -130,12 +149,12 @@ def generate_insight_by_settings(data_directory, directory_name):
 
     
 
-def generate_insight_by_resolution(data_directory, directory_name):
+def generate_insight_by_resolution(data_directory, directory_name = None):
     def filename_condition(filename):
         return filename.split('_')[1]
 
     # Process files in the given directory
-    resolutions, sum_powers = process_directory(data_directory, filename_condition)
+    resolutions, sum_powers = process_directory(data_directory, filename_condition) if directory_name else process_directory_all(data_directory, filename_condition)
     avg_powers = get_avg_power(resolutions, sum_powers)
 
     # Plot the average power for each resolution
@@ -143,14 +162,14 @@ def generate_insight_by_resolution(data_directory, directory_name):
                    'Average Power Consumption for Different Resolutions', directory_name)
 
 
-def generate_insight_by_codec_bitrate(data_directory, directory_name):
+def generate_insight_by_codec_bitrate(data_directory, directory_name = None):
     def filename_condition(filename):
         codec, bitrate = filename.split('_')[3:5]
         bitrate = normalize_codec(bitrate)
         return f'{codec} x {bitrate}'
 
     # Process files in the given directory
-    codec_bitrate_combinations, sum_powers = process_directory(data_directory, filename_condition)
+    codec_bitrate_combinations, sum_powers = process_directory(data_directory, filename_condition) if directory_name else process_directory_all(data_directory, filename_condition)
     avg_powers = get_avg_power(codec_bitrate_combinations, sum_powers)
 
     # Plot the average power for each combination of codec and bitrate
@@ -158,23 +177,23 @@ def generate_insight_by_codec_bitrate(data_directory, directory_name):
                    'Average Power Consumption for Different Bitrate x Codec Combinations', directory_name)
 
 # write a method to generate insights by bitrate
-def generate_insight_by_bitrate(data_directory, directory_name):
+def generate_insight_by_bitrate(data_directory, directory_name = None):
     def filename_condition(filename):
         return filename.split('_')[3]
 
     # Process files in the given directory
-    bitrates, sum_powers = process_directory(data_directory, filename_condition)
+    bitrates, sum_powers = process_directory(data_directory, filename_condition) if directory_name else process_directory_all(data_directory, filename_condition)
     avg_powers = get_avg_power(bitrates, sum_powers)
 
     plot_avg_power(avg_powers, 'Bitrate',
                    'Average Power Consumption for Different Bitrates', directory_name)
 
-def generate_insights_by_codec(data_directory, directory_name):
+def generate_insights_by_codec(data_directory, directory_name = None):
     def filename_condition(filename):
         return normalize_codec(filename.split('_')[4])
 
     # Process files in the given directory
-    codecs, sum_powers = process_directory(data_directory, filename_condition)
+    codecs, sum_powers = process_directory(data_directory, filename_condition) if directory_name else process_directory_all(data_directory, filename_condition)
     avg_powers = get_avg_power(codecs, sum_powers)
 
     plot_avg_power(avg_powers, 'Codec',
@@ -192,21 +211,11 @@ def generate_insight_for_directories(data_directory):
             generate_insights_by_codec(directory_path, directory)
 
 def generate_overall_insights(data_directory):
-    all_sum_powers = {}
-    for directory in os.listdir(data_directory):
-        directory_path = os.path.join(data_directory, directory)
-        if os.path.isdir(directory_path):
-            print(f'Processing {directory}')
-            _, sum_powers = process_directory(directory_path, lambda x: x)
-            for key in sum_powers:
-                if key not in all_sum_powers:
-                    all_sum_powers[key] = []
-                all_sum_powers[key].extend(sum_powers[key])
-    avg_powers = get_avg_power(all_sum_powers.keys(), all_sum_powers)
-    plot_avg_power(avg_powers, 'Settings (Codec_Resolution_Bitrate_FPS)',
-                   'Average Power Consumption for Different Settings', 'overall')
-    df = create_dataframe(avg_powers)
-    plot_corr_heat_map(df, 'overall')
+            generate_insight_by_settings(data_directory)
+            generate_insight_by_resolution(data_directory)
+            generate_insight_by_codec_bitrate(data_directory)
+            generate_insight_by_bitrate(data_directory)
+            generate_insights_by_codec(data_directory)
 
 if __name__ == "__main__":
     # Path to the directory containing the data files
@@ -214,4 +223,4 @@ if __name__ == "__main__":
 
     # Generate insights
     generate_insight_for_directories(data_directory)
-    #generate_overall_insights(data_directory)
+    generate_overall_insights(data_directory)
